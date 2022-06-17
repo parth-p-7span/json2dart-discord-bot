57674ead6d478baac00814f24f92a92a68fc0ba8
parthp-7span <parth.p@7span.com>
2022-06-17 17:54:19 +0530

    added constant class name

--- main.py
+++ main.py
@@ -26,34 +26,31 @@ async def test(ctx):
 @client.event
 async def on_message(message):
     string = message.content
-    # try:
-    data = json.loads(string)
-    print(data)
-    class_name = data['class_name']
-    del data['class_name']  # now the data variable contains only fields
+    try:
+        data = json.loads(string)
+        final_dart_code = ""
+        class_name = "DataClass"
 
-    final_dart_code = ""
+        generator = Generator(class_name, data)
+        final_dart_code += generator.create_class() + "\n\n"
 
-    generator = Generator(class_name, data)
-    final_dart_code += generator.create_class() + "\n\n"
+        for key, value in data.items():
+            if type(value) == dict:
+                temp_generator = Generator(key.capitalize(), value)
+                final_dart_code += temp_generator.create_class()
+            elif type(value) == list:
+                dtype = type(value[0])
+                if dtype == dict:
+                    temp_generator = Generator(key.capitalize(), value[0])
+                    final_dart_code += temp_generator.create_class() + "\n\n"
 
-    for key, value in data.items():
-        if type(value) == dict:
-            temp_generator = Generator(key.capitalize(), value)
-            final_dart_code += temp_generator.create_class()
-        elif type(value) == list:
-            dtype = type(value[0])
-            if dtype == dict:
-                temp_generator = Generator(key.capitalize(), value[0])
-                final_dart_code += temp_generator.create_class() + "\n\n"
-
-    file_name = class_name.lower() + ".dart"
-    with open(file_name, "w") as f:
-        f.write(final_dart_code)
-    await message.channel.send(file=discord.File(file_name))
-    os.remove(file_name)
-    # except Exception as e:
-    #     print(e)
+        file_name = class_name.lower() + ".dart"
+        with open(file_name, "w") as f:
+            f.write(final_dart_code)
+        await message.channel.send(file=discord.File(file_name))
+        os.remove(file_name)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
