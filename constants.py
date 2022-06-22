bbc1cd4ca7854a2b72968a938cdc144d592504b6
parthp-7span <parth.p@7span.com>
2022-06-22 19:53:56 +0530

    discord token updated

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-TOKEN = "OTg5MDM3NTkxOTgwNTY0NDgw.G7M9Lk.zOCyWSioIm5V3II7vWguUs78TNf9frt8x3_laI"
+TOKEN = "OTg5MDM3NTkxOTgwNTY0NDgw.Gesblc.w89G4l1l9oYWHtqNik0aB-vYY6X-BKR4WAot18"
 
 # heroku ps:scale worker=0 -a json2dart-discord-bot
 # heroku ps:scale worker=1 -a json2dart-discord-bot
