3341f77934e4017e6badf2276b759530b7e6549f
parthp-7span <parth.p@7span.com>
2022-06-22 10:54:09 +0530

    discord token updated

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-TOKEN = "OTg3MDAwNDk5NzU0NjUxNjU4.GCxARa.kGStkNc9RSlyOspkRzGvgkL_Y-REETHYUNdQDE"
+TOKEN = "OTg5MDM3NTkxOTgwNTY0NDgw.G7M9Lk.zOCyWSioIm5V3II7vWguUs78TNf9frt8x3_laI"
 
 # heroku ps:scale worker=0 -a json2dart-discord-bot
 # heroku ps:scale worker=1 -a json2dart-discord-bot
