5c8575bdd2e37df98f8609b09c49302d33830e9b
parthp-7span <parth.p@7span.com>
2022-06-23 11:06:59 +0530

    token

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-TOKEN = "OTg5MDM3NTkxOTgwNTY0NDgw.GT6HBb.kjVwNed4we0PG9Vgkez5QvQtS-ah9QKWdoE6UY"
+TOKEN = "OTg5MDM3NTkxOTgwNTY0NDgw.GUqd5d.AaaCHZREjIMVCuZSoU2avovmj2y9T-JNI9pQRY"
 
 # heroku ps:scale worker=0 -a json2dart-discord-bot
 # heroku ps:scale worker=1 -a json2dart-discord-bot
