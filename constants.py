aab6e8aa579b49a5adbb7ce532522de3975c5df6
parthp-7span <parth.p@7span.com>
2022-06-23 10:57:01 +0530

    token

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-TOKEN = "OTg5MDM3NTkxOTgwNTY0NDgw.GimI5P.D1llMdXVZY7N1HZUyKNihXnAb7o-r_iw6u7JJM"
+TOKEN = "OTg5MDM3NTkxOTgwNTY0NDgw.GT6HBb.kjVwNed4we0PG9Vgkez5QvQtS-ah9QKWdoE6UY"
 
 # heroku ps:scale worker=0 -a json2dart-discord-bot
 # heroku ps:scale worker=1 -a json2dart-discord-bot
--- main.py
+++ main.py
@@ -99,4 +99,4 @@ async def on_message(message):
 
 
 if __name__ == "__main__":
-    client.run(r"OTg5MDM3NTkxOTgwNTY0NDgw.GGmI-I.4fyA6onU5FATHZKkXW3vZxS21fa61cooqMVDcg")
+    client.run(constants.TOKEN)
